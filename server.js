354e3a6b12819c501ee010205f8481de501aea25
SpecterGate <cookiesarenotavailablerightnow@outlook.com>
2026-03-23 08:37:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 3000; 
 
 app.use('/', createProxyMiddleware({
-    target: 'https://wednesday-searches-preferred-drinks.trycloudflare.com/',
+    target: 'https://lancaster-earlier-endangered-timothy.trycloudflare.com',
     changeOrigin: true,
     ws: true, // Support for WebSockets
     onProxyRes: function (proxyRes, req, res) {
