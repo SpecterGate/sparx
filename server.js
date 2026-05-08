f258337883e23e7b5974e58ff06cfafeb4b94de2
SpecterGate <cookiesarenotavailablerightnow@outlook.com>
2026-05-08 08:05:39 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const TARGET_URL = 'https://voidagon.co.uk';
+const TARGET_URL = 'https://voidagon.co.uk/embed';
 
 app.get('/', (req, res) => {
     // This sends only the iframe with no verification UI
