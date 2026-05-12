6d55ee988e3f698fb26a294b9abfe8a945e08270
SpecterGate <cookiesarenotavailablerightnow@outlook.com>
2026-05-12 07:39:01 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,5 @@
 const express = require('express');
-const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
 const app = express();
-
-const TARGET_URL = 'https://sparx-s7fh.onrender.com/embed';
-
-app.use('/proxy', createProxyMiddleware({
-    target: TARGET_URL,
-    changeOrigin: true,
-    selfHandleResponse: true,
-    on: {
-        proxyReq: (proxyReq) => {
-            // FORCE the target server to send plain text (no GZIP/Brotli)
-            proxyReq.setHeader('accept-encoding', 'identity');
-        },
-        proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-            const contentType = proxyRes.headers['content-type'] || '';
-
-            // ONLY modify HTML files
-            if (contentType.includes('text/html')) {
-                const response = responseBuffer.toString('utf8');
-                return response
-                    .replace(/<div id="whitelist-prompt-modal"[\s\S]*?<\/div>/, '')
-                    .replace(/<div id="wl-blobs"[\s\S]*?<\/div>/, '');
-            }
-
-            // Return CSS, JS, and Socket.io files exactly as they are
-            return responseBuffer;
-        }),
-    },
-}));
 
 app.get('/', (req, res) => {
     res.send(`
